1e1422d036dcaf42a047c3e33ce3321e461354e2
tapiastj <tapiastj@gmail.com>
2018-04-21 08:59:24 -0500

    firts push

---
+++ app/src/main/java/com/example/android/taller1/principal.java
@@ -0,0 +1,13 @@
+package com.example.android.taller1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
